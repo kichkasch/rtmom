54645feee520cf1f45809db8dc1321edf8543b7b
Michael <kichkasch@gmx.de>
2010-01-07 22:34:30 +0100

    updated config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+"""
+Configuration for rtmom
+
+rtmom is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+rtmom is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with rtmom.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+"""These two are application specific rather than user specific - do not touch them"""
+api_key = "afa4ce19b3ec31058c593fbca19ab636"
+shared_secret = "d6302c3f33fcd4cf"
+
